430f5a33b6a0bc8862939f76671bf7115e67ef83
Juan Mendez <juanmndz19@gmail.com>
2016-05-14 02:37:47 +0000

    Add validations to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,21 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(post_params)
-    @post.save
-
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
+
   def show
     @post = Post.find(params[:id])
   end
-  private 
+  private
   def post_params
     params.require(:post).permit(:title, :body)
   end
